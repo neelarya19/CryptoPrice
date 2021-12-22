ffe266f9c4a7ff49f572307aba77d4c411034804
neelarya19 <neelarya19@gmail.com>
2021-12-22 17:23:52 +0530

    r

--- CryptoPrice/settings.py
+++ CryptoPrice/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[BASE_DIR / 'static']
 
 # Default primary key field type
